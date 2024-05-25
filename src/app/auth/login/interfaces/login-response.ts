d86b1870b01e39341b755d008fa7691b14bb85b0
Alisson Lima <alissongdev@gmail.com>
2024-05-25 18:20:25 -0300

    refactor: adiciona monitor ao LoginResponse

--- src/app/auth/login/interfaces/login-response.ts
+++ src/app/auth/login/interfaces/login-response.ts
@@ -1,7 +1,9 @@
+import { Monitor } from "../../../shared/interfaces/monitor";
 import { Responsavel } from "../../../shared/interfaces/responsavel";
 
 export interface LoginResponse {
   success: boolean;
   userType: string;
   responsavel?: Responsavel;
+  monitor?: Monitor;
 }

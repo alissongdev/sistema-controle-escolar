37692bd0935d2c17875a7f4cabea7bc7ac66c249
Alisson Lima <alissongdev@gmail.com>
2024-05-25 17:55:55 -0300

    feat: adiciona as rotas da aplicação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { isAuthenticatedGuard } from './auth/login/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./auth/login/login.component'),
+  },
+  {
+    path: 'home',
+    canActivate: [isAuthenticatedGuard()],
+    loadComponent: () => import('./home/home.component'),
+  },
+  {
+    path: 'recados',
+    canActivate: [isAuthenticatedGuard()],
+    loadComponent: () => import('./recados/recados.component'),
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full',
+  },
+  { 
+    path: '**', 
+    redirectTo: '/login' 
+  }
+];
